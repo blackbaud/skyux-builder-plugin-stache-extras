fd2f2a4c2babc06a8e65b4a9ba2917be905dfbfa
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-06-08 16:57:19 -0400

    Updated extras

--- src/extras.js
+++ src/extras.js
@@ -1,17 +1,31 @@
-const jsonDataPlugin = require('@blackbaud/skyux-builder-plugin-stache-json-data');
-
 const preload = (content, resourcePath) => {
   if (!resourcePath.match(/stache-extras\.module\.ts$/)) {
     return content;
   }
 
-  return content.replace(
-    'declare var StacheExtrasModule: any;',
+  // The providers listed below are automatically provided by their
+  // respective plugin.
+  return content.replace('declare var StacheExtrasModule: any;',
 `
 import { NgModule } from '@angular/core';
+
+import { SkyAppConfig } from '@blackbaud/skyux-builder/runtime';
+import { StacheModule, StacheConfigService } from '@blackbaud/stache';
+
 @NgModule({
+  imports: [
+    StacheModule
+  ],
+  exports: [
+    StacheModule
+  ],
   providers: [
-    ${jsonDataPlugin.getProviders()}
+    STACHE_JSON_DATA_PROVIDERS,
+    STACHE_ROUTE_METADATA_PROVIDERS,
+    {
+      provide: StacheConfigService,
+      useExisting: SkyAppConfig
+    }
   ]
 })
 export class StacheExtrasModule { }`);
